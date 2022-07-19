3442d873dc88b598d984ccff609db30136c7abaf
huyen_nguyen <huyen_nguyen@coccoc.com>
2022-07-19 16:55:29 +0700

    webpack clean up

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 let mode = "development";
 let target = "web";
 
@@ -12,7 +13,9 @@ if (process.env.NODE_ENV === "production") {
 module.exports = {
     mode: mode,
     target: target,
+    entry: './src/index.js',
     output: {
+        path: path.resolve(__dirname, 'dist'),
         assetModuleFilename: 'image/[hash][ext][query]'
     },
     module: {
@@ -28,7 +31,11 @@ module.exports = {
                 test: /\.jsx?$/,
                 exclude: /node_modules/,
                 use: {
-                    loader: "babel-loader"
+                    loader: "babel-loader",
+                    options: {
+                        // to avoid potential expensive babel recompilation process on each run
+                        cacheDirectory: true
+                    }
                 }
             },
             {
@@ -51,6 +58,7 @@ module.exports = {
         extensions: ['.js', '.jsx']
     },
     plugins: [
+        new CleanWebpackPlugin(),
         new MiniCssExtractPlugin(),
         new HtmlWebpackPlugin({
             template: "./src/index.html"

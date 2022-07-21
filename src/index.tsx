24037a0d9c8df5fa76c96215662c1971553211eb
Phillip Nguyen <indieretro20s@gmail.com>
2022-07-21 17:21:43 +0700

    eslint x prettier setup

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,14 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from './App';
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable @typescript-eslint/no-empty-function */
+import React from "react"
+import { createRoot } from "react-dom/client"
+import App from "./App"
 
-ReactDOM.render(<App />, document.getElementById("root"));
+// to fix react-refresh/babel problem
+;(self as any).$RefreshReg$ = () => {}
+;(self as any).$RefreshSig$ = () => () => {}
+
+const container = document.getElementById("root")
+// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+const root = createRoot(container!)
+root.render(<App />)
